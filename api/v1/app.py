5fd63c261a09f264622dc0ba3dea0a60e76e6377
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-02-01 05:12:45 -0800

    Fix typeError and adds default value

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,8 +8,8 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-HBNB_API_HOST = getenv('HBNB_API_HOST')
-HBNB_API_PORT = getenv('HBNB_API_PORT')
+HBNB_API_HOST = getenv('HBNB_API_HOST', default='0.0.0.0')
+HBNB_API_PORT = int(getenv('HBNB_API_PORT', default=5000))
 
 
 @app.errorhandler(404)
@@ -20,4 +20,4 @@ def not_found(error):
 
 if __name__ == "__main__":
     # python -m api.v1.app
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True, debug=True)
