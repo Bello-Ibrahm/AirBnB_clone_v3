20f53743de361d9853c5043621a65cd0db68004a
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-02-01 05:18:46 -0800

    Task 4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,8 @@
 """ Index view
 """
 from flask import jsonify
-
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -11,3 +11,17 @@ def status():
     """ Status of the web server
     """
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def count():
+    """ Retrieves the number of each objects by type
+    """
+    obj_types = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(obj_types)
