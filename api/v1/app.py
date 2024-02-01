66e2ec9d839ad3c741c9d1f9f7251bc76c4e84a5
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-02-01 03:49:36 -0800

    Task 3. Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Web server
+"""
+from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+HBNB_API_HOST = getenv('HBNB_API_HOST')
+HBNB_API_PORT = getenv('HBNB_API_PORT')
+
+@app.errorhandler(404)
+def not_found(error):
+    """ json 404 page """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    # python -m api.v1.app
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
