e6b2a2b2ba2cac1afd12dc02dc2216119ab19c8b
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-02-04 01:58:44 -0800

    Task 12. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,22 @@ from os import getenv
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 
+
+# Global Flask Application Variable: app
 app = Flask(__name__)
+
+# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
+
+# Output JSON responses in a human-readable(indented)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+# Cross-Origin Resource Sharing
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+
+# flask server environmental setup
 HBNB_API_HOST = getenv('HBNB_API_HOST', default='0.0.0.0')
 HBNB_API_PORT = int(getenv('HBNB_API_PORT', default=5000))
 
