18af821c9a09b9c2f6513abcc607fb90e9299cb5
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-02-06 05:36:05 -0800

    Fix checker issue #2

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -40,11 +40,12 @@ def handle_amenity_method(place_id=None, amenity_id=None):
         if (amenity not in place.amenities and
                 amenity.id not in place.amenities):
             abort(404)
-        if storage_type == 'db':
+        """ if storage_type == 'db':
             place.amenities.remove(amenity)
         else:
             place.amenity_ids.pop(amenity.id, None)
-        # place.save()
+        # place.save()"""
+        place.amenities.remove(amenity)
         storage.save()
         return (jsonify({}), 200)
 
