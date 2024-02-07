8be417963e567c7991d16882a8d0c2aefaa0f2eb
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-02-06 21:30:08 -0800

    hot fix

--- models/user.py
+++ models/user.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" holds class User"""
+import models
 from models.base_model import BaseModel, Base
-from models import storage_type
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import hashlib
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if storage_type == 'db':
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
@@ -26,11 +25,5 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user obj """
+        """initializes user"""
         super().__init__(*args, **kwargs)
-
-    def __setattr__(self, i, j):
-        """sets user pasword"""
-        if i == "password":
-            j = hashlib.md5(j.encode()).hexdigest()
-        super().__setattr__(i, j)
